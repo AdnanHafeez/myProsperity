061203eb556d2466e9774014c126272064173e54
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-02-01 20:00:00 -0800

    forgot new styles file

---
+++ src/app/commonStyles.ts
@@ -0,0 +1,25 @@
+export const foatingButtonStyle = {
+      margin: 0,
+      top: 'auto',
+      right: 20,
+      bottom: 20,
+      left: 'auto',
+      position: 'fixed'
+};
+
+export const topRightButtonStyle = {
+     position: 'relative',
+     float: 'right'
+};
+
+export const subMenuFlexContainerStyle = {
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'space-between'
+}
+
+
+export const fullWidthDialagStyle = {
+  width: '100%',
+  maxWidth: '500px',
+};
