23008c4f88ee2fbfbdebf288e46c6ccf108fcbc7
jack lightfoot <j.e.lightfoot@gmail.com>
2017-02-13 22:36:04 -0800

    stuff again

---
+++ src/app/MainContainer.ts
@@ -0,0 +1,19 @@
+import MainComponent from './MainComponent';
+import { connect } from 'react-redux';
+import {lockApplication} from './actions/security';
+import {push} from 'react-router-redux';
+
+export default connect(
+  (state) => ({
+    flash: state.view.flash
+  }),
+  (dispatch, ownProps) => {
+    return {
+      turnAppOffRedirect: (path) => {
+        console.log(path);
+        dispatch(push('/'));
+        dispatch(lockApplication(path))
+      }
+    };
+  }
+)(MainComponent);
