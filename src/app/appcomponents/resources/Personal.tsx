c47329490d83229661b205be71aee4d245e89b62
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-05-23 16:23:48 -0700

    bug# 3953 - content added

--- src/app/appcomponents/resources/Personal.tsx
+++ src/app/appcomponents/resources/Personal.tsx
@@ -13,10 +13,12 @@ const styles = {
 
   },
   container: {
-    display: 'block'
+    textAlign: 'center'
+  },
+  linkItem: {
+    margin: '5px'
   }
 };
-
 
 interface Props {
   appBarTitle(msg: string): any;
@@ -41,7 +43,44 @@ export default class Personal extends React.Component<Props, State> {
 
     return (
         <div style={styles.container as any}>
-          <h2>Coming Soon</h2>
+          <h2>Emotional Wellness: Relaxation Techniques</h2>
+
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.dartmouth.edu/~healthed/relax/downloads.html#deep">Guided Relaxation</a>
+</div>
+
+<h2>Total Fitness: Improve Your Health</h2>
+
+<div style={styles.linkItem}>
+<a target="_bank" href="https://usaphcapps.amedd.army.mil/HIOShoppingCart/viewItem.aspx?id=696">Triad: P3 Total Army Family Guide</a>
+</div>
+
+<h2>Eat Wisely</h2>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/docs/NewHandouts/Nutrition/N32_GroceryShoppingMakingAList.pdf">Grocery Shopping/Making a List</a>
+</div>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/docs/NewHandouts/Nutrition/N19_MakingAMealPlanWorkInAFamily.pdf">Making a Meal Plan Work in a Family</a>
+</div>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/docs/NewHandouts/Nutrition/N06_EatingWellOnABudget.pdf">Eating Well on a Budget</a>
+</div>
+
+
+<h2>Be Active</h2>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/download/NewHandouts/Standard/S05_IncreasingMyPhysicalActivity.pdf">How Do I Get Started With Increasing My Physical Activity?</a>
+</div>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/docs/NewHandouts/PhysicalActivity/P23_ActivitiesToFitYourLifestyle.pdf">Activities to Fit Your Lifestyle</a>
+</div>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/download/NewHandouts/Standard/S02_SetYourWeightLossGoals.pdf">Set Your Weight Loss Goals</a>
+</div>
+<div style={styles.linkItem}>
+<a target="_bank" href="https://www.move.va.gov/download/NewHandouts/Standard/S07_FITT.pdf">FITT (Frequency, Intensity, Time, and Type of Activity)</a>
+</div>
+
 
         </div>
     );
--- src/app/appcomponents/resources/Webinars.tsx
+++ src/app/appcomponents/resources/Webinars.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import { Link, browserHistory } from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
 
@@ -13,7 +12,10 @@ const styles = {
 
   },
   container: {
-    display: 'block'
+    textAlign: 'center'
+  },
+  linkItem: {
+    margin: '5px'
   }
 };
 
@@ -41,8 +43,24 @@ export default class Webinars extends React.Component<Props, State> {
 
     return (
         <div style={styles.container as any}>
-          <h2>Coming Soon</h2>
-
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p18q3afo3sn/">http://dcoe.adobeconnect.com/p18q3afo3sn/</a>
+          </div>
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p3r437gwrkl/">http://dcoe.adobeconnect.com/p3r437gwrkl/</a>
+          </div>
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p6hnn48yeu4/">http://dcoe.adobeconnect.com/p6hnn48yeu4/</a>
+          </div>
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p1eqzd8c6zb/">http://dcoe.adobeconnect.com/p1eqzd8c6zb/</a>
+          </div>
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p1pdvdvuukg/">http://dcoe.adobeconnect.com/p1pdvdvuukg/</a>
+          </div>
+          <div style={styles.linkItem}>
+            <a target="_blank" href="http://dcoe.adobeconnect.com/p66bbvuguj2/">http://dcoe.adobeconnect.com/p66bbvuguj2/</a>
+          </div>
         </div>
     );
   }
